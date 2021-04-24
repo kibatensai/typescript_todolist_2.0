613076367c5455afb2e2432a8fa96210f5d93d0c
Kibatensai <lenutd@yandex.ru>
2021-04-24 10:37:31 +0300

    Updates

---
+++ precache-manifest.c59a5f995e51fc77e376733aa1b394b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b00a3693e6abdd0f3b1d2de30404b5",
+    "url": "/typescript_todolist_2.0/index.html"
+  },
+  {
+    "revision": "b0c5a4fc286a64a4f422",
+    "url": "/typescript_todolist_2.0/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "b8b15274b1897d61fe5a",
+    "url": "/typescript_todolist_2.0/static/js/2.0498bb18.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/typescript_todolist_2.0/static/js/2.0498bb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c5a4fc286a64a4f422",
+    "url": "/typescript_todolist_2.0/static/js/main.45b7fc68.chunk.js"
+  },
+  {
+    "revision": "2dc6464b6a6ad9ea9d88",
+    "url": "/typescript_todolist_2.0/static/js/runtime-main.3071b4b1.js"
+  }
+]);
